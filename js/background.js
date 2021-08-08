730217a84fc70d49be5669b3e3186be8a2164fb4
hejo47 <hejo_47@naver.com>
2021-08-08 12:45:51 +0900

    20210808

--- js/background.js
+++ js/background.js
@@ -1,9 +1,14 @@
 const imgs = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"];
+const wrap = document.querySelector(".wrap");
 
 const chosenImg = imgs[Math.floor(Math.random() * imgs.length)];
 
 const bgImg = document.createElement("img");
 
 bgImg.src = `img/${chosenImg}`;
+console.log(chosenImg);
 
-document.body.appendChild(bgImg);
+wrap.appendChild(bgImg);
+
+
+
